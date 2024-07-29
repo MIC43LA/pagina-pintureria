fc359d1cb1e15fe4386d73016e0be2ae423c84dc
ariztegui8 <jorgeariztegui8@gmail.com>
2024-07-29 17:38:07 -0300

    change carrousel x swipwerjs

---
+++ js/js.js
@@ -0,0 +1,34 @@
+console.log('perro');
+
+const swiperEl = document.querySelector('swiper-container')
+Object.assign(swiperEl, {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    },
+});
+swiperEl.initialize();
+
