fd0660f652782a20ca91e434f18fc82715271487
ariztegui8 <jorgeariztegui8@gmail.com>
2024-07-30 07:02:56 -0300

    change mobile

--- js/js.js
+++ js/js.js
@@ -1,34 +1,46 @@
-console.log('perro');
-
-const swiperEl = document.querySelector('swiper-container')
-Object.assign(swiperEl, {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    pagination: {
-        clickable: true,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-        },
-        640: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        992: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-        },
-        1200: {
-            slidesPerView: 4,
-            spaceBetween: 20,
-        },
-    },
+$(document).ready(function () {
+    $('.carrousel').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true
+                }
+            }
+        ]
+    });
 });
-swiperEl.initialize();
-
